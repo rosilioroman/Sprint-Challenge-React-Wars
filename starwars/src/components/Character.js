1bceb31505621de988b07f9d5f835ff546257cb2
rosilioroman <rosilioroman@gmail.com>
2019-01-18 10:57:43 -0700

    Uppercased the gender prop on Character.js. Changed layout.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,7 +7,7 @@ function Character(props) {
     return (
         <div className="character">
             <div className="character-name">{props.char.name}</div>
-            <li className="character-property">Gender: {props.char.gender}</li>
+            <li className="character-property">Gender: {props.char.gender.toUpperCase()}</li>
             <li className="character-property">Eye Color: {props.char.eye_color}</li>
         </div>
     );
